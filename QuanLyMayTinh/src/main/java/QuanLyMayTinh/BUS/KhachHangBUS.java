09534fbc5b19eb8ace23b84a46afa1b3bfeb9a6a
Nguyen Chi Cong <chicongkhpro@gmail.com>
2023-04-10 23:01:13 +0700

    update code

---
+++ QuanLyMayTinh/src/main/java/QuanLyMayTinh/BUS/KhachHangBUS.java
@@ -0,0 +1,142 @@
+package QuanLyMayTinh.BUS;
+
+import QuanLyMayTinh.DAO.KhachHangDAO;
+import QuanLyMayTinh.DTO.KhachHang;
+import QuanLyMayTinh.DTO.NhanVien;
+import MyCustom.MyDialog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class KhachHangBUS {
+
+    private List<KhachHang> listKhachHang = null;
+    private KhachHangDAO khachHangDAO = new KhachHangDAO();
+
+    public void docDanhSach() {
+        this.listKhachHang = khachHangDAO.getListKhachHang();
+    }
+
+    public List<KhachHang> getListKhachHang() {
+        if (listKhachHang == null)
+            docDanhSach();
+        return listKhachHang;
+    }
+
+    public List<KhachHang> timKiemKhachHang(String txtMin, String txtMax) {
+        if (txtMax.trim().equals("") && txtMin.trim().equals(""))
+            return listKhachHang;
+        try {
+            ArrayList<KhachHang> dskh = new ArrayList<>();
+            txtMin = txtMin.replace(",", "");
+            txtMax = txtMax.replace(",", "");
+            int min = Integer.parseInt(txtMin);
+            int max = Integer.parseInt(txtMax);
+            for (KhachHang kh : listKhachHang) {
+                if (kh.getTongchitieu() >= min && kh.getTongchitieu() <= max) {
+                    dskh.add(kh);
+                }
+            }
+            return dskh;
+        } catch (Exception e) {
+            new MyDialog("Hãy nhập giá trị nguyên phù hợp!", MyDialog.ERROR_DIALOG);
+        }
+        return null;
+    }
+
+    public ArrayList<KhachHang> timKiemKhachHang(String tuKhoa) {
+        tuKhoa = tuKhoa.toLowerCase();
+        ArrayList<KhachHang> dskh = new ArrayList<>();
+        for (KhachHang kh : listKhachHang) {
+            String ho = kh.getHoKH().toLowerCase();
+            String ten = kh.getTenKH().toLowerCase();
+            int ma = kh.getMaKH();
+            if (ho.contains(tuKhoa) || ten.contains(tuKhoa) ||String.valueOf(ma).equalsIgnoreCase(tuKhoa)) {
+                dskh.add(kh);
+            }
+        }
+        return dskh;
+    }
+
+    public boolean themKhachHang(String ma, String ho, String ten, String gioiTinh) {
+        if (ten.trim().equals("") || ma.trim().equals("")) {
+            new MyDialog("Không được để trống tên!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (gioiTinh.equals("Chọn giới tính")) {
+            new MyDialog("Hãy chọn giới tính!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        int makh = Integer.parseInt(ma);
+        KhachHang kh = new KhachHang();
+        kh.setMaKH(makh);
+        kh.setHoKH(ho);
+        kh.setTenKH(ten);
+        kh.setGioitinh(gioiTinh);
+        kh.setTongchitieu(0);
+        boolean flag = khachHangDAO.addKhachHang(kh);
+        if (flag) {
+            new MyDialog("Thêm thành công!", MyDialog.SUCCESS_DIALOG);
+        } else {
+            new MyDialog("Thêm thất bại!", MyDialog.ERROR_DIALOG);
+        }
+        return flag;
+    }
+
+    public boolean suaKhachHang(String ma, String ho, String ten, String gioiTinh,int tongchitieu) {
+        if (ten.trim().equals("")||ma.trim().equals("")||ho.trim().equals("")) {
+            new MyDialog("Không được để trống tên!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        if (gioiTinh.equals("Chọn giới tính")) {
+            new MyDialog("Hãy chọn giới tính!", MyDialog.ERROR_DIALOG);
+            return false;
+        }
+        int makh = Integer.parseInt(ma);
+        KhachHang kh = new KhachHang();
+        kh.setMaKH(makh);
+        kh.setHoKH(ho);
+        kh.setTenKH(ten);
+        kh.setGioitinh(gioiTinh);
+        kh.setTongchitieu(tongchitieu);
+        boolean flag = khachHangDAO.updateKhachHang(makh,kh);
+        if (flag) {
+            new MyDialog("Sửa thành công!", MyDialog.SUCCESS_DIALOG);
+        } else {
+            new MyDialog("Sửa thất bại!", MyDialog.ERROR_DIALOG);
+        }
+        return flag;
+    }
+
+    public boolean xoaKhachHang(String ma) {
+        boolean flag = false;
+        try {
+            int maKH = Integer.parseInt(ma);
+            MyDialog dlg = new MyDialog("Bạn có chắc chắn muốn xoá?", MyDialog.WARNING_DIALOG);
+            if(dlg.getAction() == MyDialog.CANCEL_OPTION)
+                return false;
+            flag = khachHangDAO.deleteKhachHang(maKH);
+        } catch (Exception e) {
+            new MyDialog("Chưa chọn khách hàng!", MyDialog.ERROR_DIALOG);
+        }
+        if (flag) {
+            new MyDialog("Xoá thành công!", MyDialog.SUCCESS_DIALOG);
+        } else {
+            new MyDialog("Xoá thất bại!", MyDialog.ERROR_DIALOG);
+        }
+        return flag;
+    }
+    public boolean updateTongChiTieu(String maKH, String tongChiTieu) {
+    	boolean flag = false;
+    	int ma = Integer.parseInt(maKH);
+    	int tct = Integer.parseInt(tongChiTieu);
+    	
+    	flag = khachHangDAO.updateTongChiTieu(ma, tct);
+    	if(flag) {
+    		return true;
+    	}
+    	return false;
+    }
+
+}
---
+++ QuanLyMayTinh/src/main/java/QuanLyMayTinh/DAO/KhachHangDAO.java
@@ -0,0 +1,175 @@
+/*
+
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package QuanLyMayTinh.DAO;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import QuanLyMayTinh.DTO.KhachHang;
+
+/**
+ * @author User
+ */
+public class KhachHangDAO {
+
+	public ArrayList<KhachHang> getListKhachHang() {
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		ArrayList<KhachHang> dskh = new ArrayList<KhachHang>();
+		try {
+			session.getTransaction().begin();
+			String sql = "Select e from " + KhachHang.class.getName() + " e";
+			Query<KhachHang> query = session.createQuery(sql);
+			List<KhachHang> danhsachkhachhang = query.getResultList();
+			for (KhachHang kh : danhsachkhachhang) {
+				KhachHang kh1 = new KhachHang();
+				kh1.setMaKH(kh.getMaKH());
+				kh1.setHoKH(kh.getHoKH());
+				kh1.setTenKH(kh.getTenKH());
+				kh1.setGioitinh(kh.getGioitinh());
+				kh1.setTongchitieu(kh.getTongchitieu());
+				dskh.add(kh1);
+			}
+			session.getTransaction().commit();
+			return dskh;
+		} catch (Exception ex) {
+			session.getTransaction().rollback();
+		}
+		return dskh;
+
+	}
+
+	public KhachHang getKhachHang(int maKH) {
+		KhachHang kh = null;
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		try {
+			session.getTransaction().begin();
+			String sql = "Select e from " + KhachHang.class.getName() + " e where e.maKH = :maKH";
+			Query<KhachHang> query = session.createQuery(sql);
+			query.setParameter("maKH", maKH);
+			kh = query.getSingleResult();
+			session.getTransaction().commit();
+			return kh;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			session.getTransaction().rollback();
+			return null;
+		}
+	}
+
+//Works
+	public boolean addKhachHang(KhachHang kh) {
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		try {
+			session.getTransaction().begin();
+			String sql = "INSERT INTO khachhang VALUES(:MaKH,:Ho,:Ten,:GioiTinh,:TongChiTieu,1)";
+			Query<KhachHang> query = session.createNativeQuery(sql);
+			query.setParameter("MaKH", kh.getMaKH());
+			query.setParameter("Ho", kh.getHoKH());
+			query.setParameter("Ten", kh.getTenKH());
+			query.setParameter("GioiTinh", kh.getGioitinh());
+			query.setParameter("TongChiTieu", kh.getTongchitieu());
+			int count = query.executeUpdate();
+			System.out.println(count);
+			if (count > 0) {
+				session.getTransaction().commit();
+				System.out.println("abc");
+				return true;
+			} else {
+				session.clear();
+				session.close();
+
+				return false;
+
+			}
+		} catch (Exception e) {
+		}
+		return false;
+	}
+
+//Yay, it worked
+	public boolean deleteKhachHang(int maKH) {
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		try {
+			session.getTransaction().begin();
+			String sql = "Delete from " + KhachHang.class.getName() + " d" + " where d.maKH like :MaKH";
+			Query<KhachHang> query = session.createQuery(sql);
+			query.setParameter("MaKH", maKH);
+			int count = query.executeUpdate();
+			System.out.println(count);
+			if (count > 0) {
+				session.getTransaction().commit();
+				return true;
+			} else {
+				session.clear();
+				session.close();
+				return false;
+			}
+		} catch (Exception ex) {
+			return false;
+		}
+	}
+
+//Works
+	public boolean updateKhachHang(int maKH, KhachHang kh) {
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		try {
+			session.getTransaction().begin();
+			String sql = "UPDATE khachhang SET Ho=:Ho, Ten=:Ten, GioiTinh=:GioiTinh WHERE MaKH=:MaKH";
+			Query<KhachHang> query = session.createNativeQuery(sql);
+			query.setParameter("Ho", kh.getHoKH());
+			query.setParameter("Ten", kh.getTenKH());
+			query.setParameter("GioiTinh", kh.getGioitinh());
+			query.setParameter("MaKH", maKH);
+			int count = query.executeUpdate();
+			System.out.println(count);
+			if (count > 0) {
+				session.getTransaction().commit();
+				return true;
+			} else {
+				session.clear();
+				session.close();
+				return false;
+			}
+		} catch (Exception ex) {
+			return false;
+		}
+	}
+
+//Thumbs up
+	public boolean updateTongChiTieu(int maKH, int tongChiTieu) {
+		boolean result = false;
+		SessionFactory factory = HibernateUtils.getSessionFactory();
+		Session session = factory.getCurrentSession();
+		try {
+			session.getTransaction().begin();
+//      String sql = "UPDATE khachhang SET TongChiTieu=" + tongChiTieu + " WHERE MaKH=" + maKH;
+			String sql = "UPDATE khachhang SET TongChiTieu= :TongChiTieu WHERE MaKH= :MaKH";
+			Query<KhachHang> query = session.createNativeQuery(sql);
+			query.setParameter("TongChiTieu", tongChiTieu);
+			query.setParameter("MaKH", maKH);
+			int count = query.executeUpdate();
+			System.out.println(count);
+			if (count > 0) {
+				session.getTransaction().commit();
+				return true;
+			} else {
+				session.clear();
+				session.close();
+				return false;
+			}
+		} catch (Exception ex) {
+			return false;
+		}
+	}
+}
